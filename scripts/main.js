51a99e91308050bdd092281f8929cde82756fbfd
fwtravisbain <fwtravisbain@outlook.com>
2019-02-25 21:58:33 -0800

    Button and Text Updating
    Fixed the refresh button and the text now properly updates.

--- scripts/main.js
+++ scripts/main.js
@@ -3,30 +3,30 @@ var myCard2 = document.getElementById("card2");
 var myCard3 = document.getElementById("card3");
 var myCard4 = document.getElementById("card4");
 var myCard5 = document.getElementById("card5");
-//var myNewHandButton = document.querySelecter("button");
+var myNewHandButton = document.getElementById("newhandbutton");
 
 
-myCard1.innerHTML = "empty1";
-myCard2.innerHTML = "empty2";
-myCard3.innerHTML = "empty3";
-myCard4.innerHTML = "empty4";
-myCard5.innerHTML = "empty5";
+myCard1.innerHTML = "empty";
+myCard2.innerHTML = "empty";
+myCard3.innerHTML = "empty";
+myCard4.innerHTML = "empty";
+myCard5.innerHTML = "empty";
 
 //Draw a new hand
-//function drawNewHand()
-//{
-//	/*var myNumber = prompt('Please enter a number.');
-//	localStorage.setItem('number', myName);
-//	myHeading.textContent = 'Your number is, ' + myNumber;*/
-//	
-//	myCard1.innerHTML = 'Card 1';
-//	myCard2.innerHTML = 'Card 2';
-//	myCard3.innerHTML = 'Card 3';
-//	myCard4.innerHTML = 'Card 4';
-//	myCard5.innerHTML = 'Card 5';
-//}
-//
-//myNewHandButton.onclick = function()
-//{
-//	drawNewHand();
-//}
+function drawNewHand()
+{
+	/*var myNumber = prompt('Please enter a number.');
+	localStorage.setItem('number', myName);
+	myHeading.textContent = 'Your number is, ' + myNumber;*/
+	
+	myCard1.innerHTML = 'Card 1';
+	myCard2.innerHTML = 'Card 2';
+	myCard3.innerHTML = 'Card 3';
+	myCard4.innerHTML = 'Card 4';
+	myCard5.innerHTML = 'Card 5';
+}
+
+myNewHandButton.onclick = function()
+{
+	drawNewHand();
+}
