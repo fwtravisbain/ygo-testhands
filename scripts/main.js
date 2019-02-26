1518c04a9d19aceba1d3fa6f894e1dc59a8d824d
fwtravisbain <fwtravisbain@outlook.com>
2019-02-25 21:31:10 -0800

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 var myCard1 = document.getElementById('card1');
-var myCard1 = document.getElementById('card2');
-var myCard1 = document.getElementById('card3');
-var myCard1 = document.getElementById('card4');
-var myCard1 = document.getElementById('card5');
+var myCard2 = document.getElementById('card2');
+var myCard3 = document.getElementById('card3');
+var myCard4 = document.getElementById('card4');
+var myCard5 = document.getElementById('card5');
 var myNewHandButton = document.querySelecter('button');
 
 
@@ -19,11 +19,11 @@ function drawNewHand()
 	localStorage.setItem('number', myName);
 	myHeading.textContent = 'Your number is, ' + myNumber;*/
 	
-	myCard1.textContent = 'Card 1';
-	myCard2.textContent = 'Card 2';
-	myCard3.textContent = 'Card 3';
-	myCard4.textContent = 'Card 4';
-	myCard5.textContent = 'Card 5';
+	myCard1.innerHTML = 'Card 1';
+	myCard2.innerHTML = 'Card 2';
+	myCard3.innerHTML = 'Card 3';
+	myCard4.innerHTML = 'Card 4';
+	myCard5.innerHTML = 'Card 5';
 }
 
 myNewHandButton.onclick = function()
