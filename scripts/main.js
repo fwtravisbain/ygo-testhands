3e14d76fcdd1720a81de840ab6abf926eee5b57b
fwtravisbain <fwtravisbain@outlook.com>
2019-02-26 04:35:23 -0800

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ var ydkDeck = new Array;
 $.get('Burning Abyss.ydk', function(data){
         ydkDeck = data.split('\n');
         console.log(ydkDeck);
-    });
+    }, 'text');
 
 var myCard1 = document.getElementById("card1");
 var myCard2 = document.getElementById("card2");
