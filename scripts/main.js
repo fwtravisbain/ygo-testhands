fe2a875e2f8bc6e926d4500bdbe1a0f9b3c5b7a8
fwtravisbain <fwtravisbain@outlook.com>
2019-02-25 20:50:36 -0800

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,28 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myNewHandButton = document.querySelecter('button');
+
+myHeading.textContent = 'Yu-Gi-Oh! Test Hands';
+
+//Draw a new hand
+function drawNewHand()
+{
+	var myNumber = prompt('Please enter a number.');
+	localStorage.setItem('number', myName);
+	myHeading.textContent = 'Your number is, ' + myNumber;
+}
+
+//Initialize
+if(!localStorage.getItem('name'))
+{
+	drawNewHand();
+}
+else
+{
+	var storedNumber = localStorage.getItem('number');
+	myHeading.textContent = 'Your number is, ' + storedNumber;
+}
+
+myNewHandButton.onclick = function()
+{
+	drawNewHand();
+}
