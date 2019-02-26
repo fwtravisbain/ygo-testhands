a1d86854d270ef21eebd550cefa124fc68ceca4f
fwtravisbain <fwtravisbain@outlook.com>
2019-02-25 21:25:48 -0800

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
-var myCard1 = document.querySelector('card1');
-var myCard2 = document.querySelector('card2');
-var myCard3 = document.querySelector('card3');
-var myCard4 = document.querySelector('card4');
-var myCard5 = document.querySelector('card5');
+var myCard1 = document.getElementById('card1');
+var myCard1 = document.getElementById('card2');
+var myCard1 = document.getElementById('card3');
+var myCard1 = document.getElementById('card4');
+var myCard1 = document.getElementById('card5');
 var myNewHandButton = document.querySelecter('button');
 
 
 myCard1.innerHTML = 'empty1';
-document.getElementById("card2").innerHTML = 'empty';
-myCard3.textContent = 'empty';
-myCard4.textContent = 'empty';
-myCard5.textContent = 'empty';
+myCard2.innerHTML = 'empty2';
+myCard3.innerHTML = 'empty3';
+myCard4.innerHTML = 'empty4';
+myCard5.innerHTML = 'empty5';
 
 //Draw a new hand
 function drawNewHand()
