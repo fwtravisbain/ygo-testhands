f0dfd93faa895d93c667e433fedabf334114669c
fwtravisbain <fwtravisbain@outlook.com>
2019-02-25 21:34:13 -0800

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,32 @@
-var myCard1 = document.getElementById('card1');
-var myCard2 = document.getElementById('card2');
-var myCard3 = document.getElementById('card3');
-var myCard4 = document.getElementById('card4');
-var myCard5 = document.getElementById('card5');
-var myNewHandButton = document.querySelecter('button');
+var myCard1 = document.getElementById("card1");
+var myCard2 = document.getElementById("card2");
+var myCard3 = document.getElementById("card3");
+var myCard4 = document.getElementById("card4");
+var myCard5 = document.getElementById("card5");
+var myNewHandButton = document.querySelecter("button");
 
 
-myCard1.innerHTML = 'empty1';
-myCard2.innerHTML = 'empty2';
-myCard3.innerHTML = 'empty3';
-myCard4.innerHTML = 'empty4';
-myCard5.innerHTML = 'empty5';
+myCard1.innerHTML = "empty1";
+myCard2.innerHTML = "empty2";
+myCard3.innerHTML = "empty3";
+myCard4.innerHTML = "empty4";
+myCard5.innerHTML = "empty5";
 
 //Draw a new hand
-function drawNewHand()
-{
-	/*var myNumber = prompt('Please enter a number.');
-	localStorage.setItem('number', myName);
-	myHeading.textContent = 'Your number is, ' + myNumber;*/
-	
-	myCard1.innerHTML = 'Card 1';
-	myCard2.innerHTML = 'Card 2';
-	myCard3.innerHTML = 'Card 3';
-	myCard4.innerHTML = 'Card 4';
-	myCard5.innerHTML = 'Card 5';
-}
-
-myNewHandButton.onclick = function()
-{
-	drawNewHand();
-}
+//function drawNewHand()
+//{
+//	/*var myNumber = prompt('Please enter a number.');
+//	localStorage.setItem('number', myName);
+//	myHeading.textContent = 'Your number is, ' + myNumber;*/
+//	
+//	myCard1.innerHTML = 'Card 1';
+//	myCard2.innerHTML = 'Card 2';
+//	myCard3.innerHTML = 'Card 3';
+//	myCard4.innerHTML = 'Card 4';
+//	myCard5.innerHTML = 'Card 5';
+//}
+//
+//myNewHandButton.onclick = function()
+//{
+//	drawNewHand();
+//}
